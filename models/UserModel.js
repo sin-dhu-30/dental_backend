4b257195a737e6eb9a9ea7c360167f83b91918ad
Sindhu J <sindhu30sindhu30official@gmail.com>
2025-06-30 10:24:05 +0530

    Fix syntax error in UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -23,35 +23,7 @@ const userSchema = new mongoose.Schema(
   },
 )
 
-const User = mongoose.model(const mongoose = require("mongoose")
-
-const userSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true, // no duplicate emails
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true, // automatically adds createdAt and updatedAt fields
-  },
-)
-
 // Prevent model re-compilation in serverless environment
 const User = mongoose.models.User || mongoose.model("User", userSchema)
-
-module.exports = User
-"User", userSchema)
 
 module.exports = User
